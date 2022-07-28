180431e23acd15405e39f811d554dd4605fe8f35
devcrema <devcrema@gmail.com>
2022-07-29 03:24:33 +0900

    :tada: init project

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+    id("org.jetbrains.intellij") version "1.7.0"
+}
+
+group = "io.github.devcrema"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2021.2.4")
+}
+tasks {
+    patchPluginXml {
+        changeNotes.set("""
+            Add change notes here.<br>
+            <em>most HTML tags may be used</em>        """
+            .trimIndent())
+    }
+}
